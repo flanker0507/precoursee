7685bf0808922e826d101e873d85002cde1aa688
sekrupman <yayasdm@gmail.com>
2024-07-28 11:55:35 +0700

    add OOP Javascript

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-//FUNCTION MENCARI NILAI 9 DI ARRAY
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20];
-
-function findNum(arr) {
-    let found = false;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 9) {
-            found = true;
-            break;
+class Kendaraan {
+    constructor(merk, model) {
+        if (this.constructor === Kendaraan) {
+            throw new Error("Abstract class tidak dapat diinstansiasi langsung.");
         }
+        this.merk = merk;
+        this.model = model;
     }
-    if (found) {
-        console.log("ada angka 9");
-    } else {
-        console.log("tidak ada angka 9");
+
+    deskripsi() {
+        throw new Error("Metode 'deskripsi()' harus diimplementasikan dalam subclass.");
     }
 }
+
+class Mobil extends Kendaraan {         //INHERITANCE
+    constructor(merk, model, tipe) {
+        super(merk, model);             //INHERITANCE
+        this.tipe = tipe;               //POLYMORPHISM
+    }
+
+    deskripsi() {
+        return `${this.merk} ${this.model} adalah mobil tipe ${this.tipe}.`;    //ABSTRACTION
+    }
+}
+
+const mobil1 = new Mobil("Toyota", "Corolla", "Sedan");
+const mobil2 = new Mobil("Honda", "HRV", "Minivan");
+console.log(mobil1.deskripsi());
+console.log(mobil2.deskripsi());
