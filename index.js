77a9f82eefd13c6eb912e39365f70621775adc53
sekrupman <yayasdm@gmail.com>
2024-07-28 11:07:02 +0700

    adding some array

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //FUNCTION MENCARI NILAI 9 DI ARRAY
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20];
 
 function findNum(arr) {
     let found = false;
